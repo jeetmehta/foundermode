90e0729ff7ada24e27dd9b5e98634bdac0512407
Jeet Mehta <jeet25.m@gmail.com>
2024-10-12 19:14:58 -0400

    phone number input flag

--- src/screens/CreateAccountScreen.tsx
+++ src/screens/CreateAccountScreen.tsx
@@ -1,7 +1,7 @@
 import { useSignUp } from "@clerk/clerk-expo";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import {
   Alert,
   StyleSheet,
@@ -10,6 +10,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import PhoneInput from "react-native-phone-number-input";
 
 const CreateAccountScreen: React.FC = () => {
   const { signUp, setActive } = useSignUp();
@@ -17,6 +18,7 @@ const CreateAccountScreen: React.FC = () => {
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [isEmailMode, setIsEmailMode] = useState(true);
+  const phoneInput = useRef<PhoneInput>(null);
 
   const handleSignUp = async () => {
     if (!signUp) {
@@ -119,21 +121,44 @@ const CreateAccountScreen: React.FC = () => {
       <Text style={styles.title}>Create an account</Text>
 
       <View style={styles.inputContainer}>
-        <Ionicons
-          name={isEmailMode ? "mail-outline" : "call-outline"}
-          size={24}
-          color="#888"
-          style={styles.icon}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder={isEmailMode ? "Email" : "Phone Number"}
-          placeholderTextColor="#888"
-          value={isEmailMode ? email : phoneNumber}
-          onChangeText={isEmailMode ? setEmail : setPhoneNumber}
-          keyboardType={isEmailMode ? "email-address" : "phone-pad"}
-        />
+        {isEmailMode && (
+          <>
+            <Ionicons
+              name={isEmailMode ? "mail-outline" : "call-outline"}
+              size={24}
+              color="#888"
+              style={styles.icon}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder={isEmailMode ? "Email" : "Phone Number"}
+              placeholderTextColor="#888"
+              value={isEmailMode ? email : phoneNumber}
+              onChangeText={isEmailMode ? setEmail : setPhoneNumber}
+              keyboardType={isEmailMode ? "email-address" : "phone-pad"}
+            />
+          </>
+        )}
       </View>
+      {!isEmailMode && (
+        <PhoneInput
+          ref={phoneInput}
+          defaultValue={phoneNumber}
+          defaultCode="US"
+          layout="first"
+          onChangeText={(text) => {
+            setPhoneNumber(text);
+          }}
+          onChangeFormattedText={(text) => {
+            setPhoneNumber(text);
+          }}
+          withDarkTheme
+          withShadow
+          autoFocus
+          containerStyle={styles.phoneInputContainer}
+          textContainerStyle={styles.phoneInputTextContainer}
+        />
+      )}
       <TouchableOpacity style={styles.button} onPress={handleSignUp}>
         <Text style={styles.buttonText}>Continue</Text>
       </TouchableOpacity>
@@ -242,6 +267,13 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 16,
     marginVertical: 10,
+  },
+  phoneInputContainer: {
+    width: "100%",
+    marginBottom: 10,
+  },
+  phoneInputTextContainer: {
+    backgroundColor: "#fff",
   },
 });
 
